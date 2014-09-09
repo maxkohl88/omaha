2db3b572096b6f2e07be2ff271fa525da020040a
Max Kohl <maxkohl810@gmail.com>
2014-09-09 17:12:58 -0400

    offensive plays controller now serves up play info via json

---
+++ app/serializers/offensive_play_serializer.rb
@@ -0,0 +1,3 @@
+class OffensivePlaySerializer < ActiveModel::Serializer
+  attributes :id, :play_name, :play_type, :player1_coordinates, :player2_coordinates, :player3_coordinates, :player4_coordinates, :player5_coordinates, :player6_coordinates, :player7_coordinates, :player8_coordinates, :player9_coordinates, :player10_coordinates, :player11_coordinates
+end
